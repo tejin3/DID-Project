dad197107a33808fe869d2c791c59aff2de3ebbf
EUNKYUNG JUNG <thxjek@gmail.com>
2021-10-18 16:30:58 +0900

    Update trade.vue

--- client/src/layouts/default/Navbar.vue
+++ client/src/layouts/default/Navbar.vue
@@ -1,31 +1,64 @@
 <template>
-    <v-app-bar app color="primary">
-        <!-- icon-mdi-menu -->
-        <v-app-bar-nav-icon @click="$emit('drawer')"></v-app-bar-nav-icon>
+    <div>
+        <v-app-bar app>
+            <v-toolbar-title>Awesome Shop</v-toolbar-title>
 
-        <!-- <v-spacer /> -->
-        <!-- <parallax class="page-header header-filter" :style="headerStyle">
-        </parallax> -->
-    </v-app-bar>
+            <v-spacer></v-spacer>
+
+            <span class="hidden-sm-and-up">
+                <v-btn @click.stop="drawer = !drawer">
+                    Menu
+                </v-btn>
+            </span>
+
+            <v-toolbar-items class="hidden-xs-only">
+                <v-btn to="/" text>
+                    <v-icon small left>fa-home</v-icon>
+                    Home
+                </v-btn>
+                <v-btn to="store" text>
+                    <v-icon small left>fa-shopping-basket</v-icon>
+                    Store
+                </v-btn>
+                <v-btn to="cart" text>
+                    <v-icon small left>fa-shopping-cart</v-icon>
+                    Cart
+                </v-btn>
+            </v-toolbar-items>
+        </v-app-bar>
+
+        <v-navigation-drawer v-model="drawer" absolute temporary right>
+            <v-list>
+                <v-list-item
+                    v-for="item in items"
+                    :key="item.title"
+                    :to="item.link"
+                    link
+                >
+                    <v-list-item-icon>
+                        <v-icon small>fa-{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </div>
 </template>
 
 <script>
-export default {
+module.exports = {
     name: 'DefaultBar',
-    props: {
-        image: {
-            type: String,
-            default: require('@/assets/img/vue-mk-header.jpg')
-        }
-    },
-    computed: {
-        headerStyle() {
-            return {
-                backgroundImage: `url(${this.image})`
-            }
+    data() {
+        return {
+            drawer: false,
+            items: [
+                { title: 'Home', link: '/', icon: 'home' },
+                { title: 'Store', link: 'store', icon: 'shopping-basket' },
+                { title: 'Cart', link: 'cart', icon: 'shopping-cart' }
+            ]
         }
     }
 }
 </script>
-
-<style></style>
--- client/src/views/Trade.vue
+++ client/src/views/Trade.vue
@@ -1,41 +1,56 @@
 <template>
-    <div>
+    <v-container fluid>
         <v-container>
-            <v-card fluid class="mb-10">
+            <v-card class="mb-10" fluid>
                 <v-img src="../assets/img/banner.jpg"> </v-img>
             </v-card>
-            <v-row justify="space-around">
-                <v-hover v-slot="{ hover }" open-delay="200">
-                    <v-card
-                        width="300"
-                        :elevation="hover ? 16 : 2"
-                        :class="{ 'on-hover': hover }"
-                        class="mx-auto"
-                        max-width="350"
-                    >
-                        <v-img
-                            height="300px"
-                            src="../assets/img/bucks-cafe.png"
+        </v-container>
+
+        <!--hover했을떄 효과 및 카드-->
+        <v-container>
+            <v-row>
+                <v-col
+                    class="mr-auto"
+                    v-for="count in 4"
+                    :key="count"
+                    cols="12"
+                    sm="4"
+                    md="2"
+                >
+                    <v-hover v-slot="{ hover }" open-delay="200">
+                        <v-card
+                            width="150"
+                            :elevation="hover ? 16 : 2"
+                            :class="{ 'on-hover': hover }"
+                            class="mx-auto"
+                            max-width="150"
                         >
-                            <v-app-bar flat color="rgba(0, 0, 0, 0)">
-                            </v-app-bar>
-                        </v-img>
+                            <v-img
+                                height="150"
+                                src="../assets/img/bucks-cafe.png"
+                            >
+                                <v-app-bar flat color="rgba(0, 0, 0, 0) ">
+                                </v-app-bar>
+                            </v-img>
+                            <!--텍스트부분-->
+                            <v-card-text>
+                                <div class="text--primary">
+                                    10 coupons
+                                </div>
+                                <p>
+                                    Starbucks Americano
+                                </p>
 
-                        <v-card-text>
-                            <v-card-title>
-                                coupon 20장
-                            </v-card-title>
-
-                            <v-card-subtitle>
-                                스타벅스 아메리카노
-                            </v-card-subtitle>
-                            <div class="font-weight-bold ml-8 mb-2"></div>
-                        </v-card-text>
-                    </v-card>
-                </v-hover>
+                                <v-btn text color="deep-purple accent-4">
+                                    Trade
+                                </v-btn>
+                            </v-card-text>
+                        </v-card>
+                    </v-hover>
+                </v-col>
             </v-row>
         </v-container>
-    </div>
+    </v-container>
 </template>
 
 <script>
@@ -52,3 +67,4 @@ export default {
     methods: {}
 }
 </script>
+<style scoped></style>
