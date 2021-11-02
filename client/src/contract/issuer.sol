f81571e1fb16ace94561d0e7f8f455e3383a5a93
tejin3 <tejin3@naver.com>
2021-11-02 14:06:48 +0900

    add

---
+++ client/src/contract/issuer.sol
@@ -0,0 +1,22 @@
+pragma solidity 0.8.9;
+
+contract issuer {
+    
+    address DID;
+    string publicKey;
+    string privateKey;
+    string encryptPublickey;
+    mapping (address => bool) vcList;
+    mapping (address => string) apporval;
+    
+    constructor (string memory _publicKey, string memory _privateKey, string memory _encryptPublickey)  {
+        DID = msg.sender;
+        publicKey = _publicKey;
+        privateKey = _privateKey;
+        encryptPublickey = _encryptPublickey;
+    }
+    
+    function makeVC() public {
+        
+    }
+}
