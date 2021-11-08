69b896d5be31a3fac6d24a57cbf20ed790b38cb0
Heekyung Do <dhk0729@gmail.com>
2021-11-08 09:18:11 +0900

    main

--- client/src/views/Main.vue
+++ client/src/views/Main.vue
@@ -7,12 +7,12 @@
                     cols="12"
                     md="6"
                 >
-                    <base-bubble-1
+                    <!-- <base-bubble-1
                         style="transform: rotate(180deg) translateY(25%)"
-                    />
+                    /> -->
 
                     <v-row align="center" justify="center">
-                        <v-col cols="10" md="6">
+                        <v-col cols="10" md="6" mb="0">
                             <h2>We DID survey</h2>
                             <p class="mt-4">
                                 당신의 똑똑한 생각을 나눠주세요! 지금 서베이에
@@ -24,34 +24,34 @@
                         </v-col>
                     </v-row>
 
-                    <base-bubble-2
+                    <!-- <base-bubble-2
                         style="transform: rotate(180deg) translate(-200px, -15%)"
-                    />
+                    /> -->
                 </v-col>
 
                 <v-col md="5">
                     <MainCarousel />
                 </v-col>
-                <v-row class="py-10 px-16 mt-16">
-                    <!-- <v-row class="py-10 px-16 mt-16 pink lighten-5"> -->
-                    <v-col>
-                        <!-- <v-responsive
-                            width="500"
-                            height="300"
-                            class="px-10 explain"
-                        >
-                            <h3 class="text-h4">
-                                Try experimental new features
-                            </h3>
+                <!-- <v-row class="py-10 px-16 mt-16 pink lighten-5"> -->
+                <!-- <v-row class="py-10 px-16 mt-16">
+                <v-col>
+                    <v-responsive
+                        width="500"
+                        height="300"
+                        class="px-10 explain"
+                    >
+                        <h3 class="text-h4">
+                            Try experimental new features
+                        </h3>
 
-                            <p class="mt-4 text-medium-emphasis">
-                                For a limited time, Premium members can try out
-                                new features that we’re working on. Share your
-                                feedback to help us build a better YouTube.
-                            </p>
-                        </v-responsive> -->
-                    </v-col>
-                </v-row>
+                        <p class="mt-4 text-medium-emphasis">
+                            For a limited time, Premium members can try out new
+                            features that we’re working on. Share your feedback
+                            to help us build a better YouTube.
+                        </p>
+                    </v-responsive>
+                </v-col>
+            </v-row> -->
             </v-row>
 
             <v-row class="py-16 px-16">
