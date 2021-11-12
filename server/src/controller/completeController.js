a661aaa0ae9e79445b0ada21a156a15ad6e9a223
PastelWhale <43264739+HongDaeEui@users.noreply.github.com>
2021-11-12 10:45:25 +0900

    Update: user point $ coupon db

--- server/src/controller/completeController.js
+++ server/src/controller/completeController.js
@@ -2,15 +2,31 @@
 const mysql = require("../config/mysql");
 
 const index = async (req, res) => {
-    try {
-        const results = await mysql.query("getCompletePeople");
-        console.table(results);
-        res.status(200).send(results);
-    } catch (err) {
-        res.status(500).send({
-            error: err,
-        });
-    }
+  try {
+    const results = await mysql.query("getCompletePeople");
+    console.table(results);
+    res.status(200).send(results);
+  } catch (err) {
+    res.status(500).send({
+      error: err,
+    });
+  }
 };
 
-module.exports = { index };
+const save = async (req, res) => {
+  console.log(req);
+  console.log(req.body.param);
+  const reward = req.body.param;
+  try {
+    await mysql.query("insertReward", {
+      user_point: reward.point,
+      user_coupon: reward.coupon,
+    });
+  } catch (err) {
+    res.status(500).send({
+      error: err,
+    });
+  }
+};
+
+module.exports = { index, save };
