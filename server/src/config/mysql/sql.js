1100ba9e2d6d37bebc8b1d2f98de87464ae3b7fb
PastelWhale <43264739+HongDaeEui@users.noreply.github.com>
2021-11-12 17:10:33 +0900

    Update: selected point & coupon connect mariaDB

--- server/src/config/mysql/sql.js
+++ server/src/config/mysql/sql.js
@@ -1,31 +1,33 @@
 module.exports = {
-    getSurveys: {
-        query: "select * from t_survey",
-    },
-    getSurveyById: {
-        query: `select * from t_survey where survey_id = ?`,
-    },
-    getSurveyByIds: {
-        query: `select * from t_survey where survey_id in (?)`,
-    },
-    getSurveyByCompany: {
-        query: "SELECT t1.survey_id, t1.survey_start_date ,t1.survey_end_date ,t1.survey_title ,t1.survey_description ,(t1.survey_request_count/t1.survey_complete_count)*100 as total_complte ,t1.user_account, (Datediff(date_format(t1.survey_end_date,'%Y-%m-%d'), date_format(now(),'%Y-%m-%d'))) as dDay FROM t_survey t1  where t1.user_account = ?",
-    },
-    getCompletePeople: {
-        query: "select * from t_complete",
-    },
-    getQuestions: {
-        query: "select * from t_question where survey_id = ?",
-    },
-    insertAnswer: {
-        query: "insert into t_answer set ?",
-    },
-    discountDay: {
-        query: `
+  getSurveys: {
+    query: "select * from t_survey",
+  },
+  getSurveyById: {
+    query: `select * from t_survey where survey_id = ?`,
+  },
+  getSurveyByIds: {
+    query: `select * from t_survey where survey_id in (?)`,
+  },
+  getSurveyByCompany: {
+    query:
+      "SELECT t1.survey_id, t1.survey_start_date ,t1.survey_end_date ,t1.survey_title ,t1.survey_description ,(t1.survey_request_count/t1.survey_complete_count)*100 as total_complte ,t1.user_account, (Datediff(date_format(t1.survey_end_date,'%Y-%m-%d'), date_format(now(),'%Y-%m-%d'))) as dDay FROM t_survey t1  where t1.user_account = ?",
+  },
+  getCompletePeople: {
+    query: "select * from t_complete",
+  },
+  getQuestions: {
+    query: "select * from t_question where survey_id = ?",
+  },
+  insertAnswer: {
+    query: "insert into t_answer set ?",
+  },
+  discountDay: {
+    query: `
     select Datediff(T2.survey_end_date, now()) as "dday"
     from t_survey T2`,
-    },
-    insertReward: {
-        query: "insert into t_user set ?",
-    },
+  },
+  updateReward: {
+    query:
+      "update t_user set user_point = user_point + ?, user_coupon = user_coupon + ? where user_account = ? ",
+  },
 };
