24a2b3ced76200044c1fcf9cb96dcfb7156dcd77
PastelWhale <43264739+HongDaeEui@users.noreply.github.com>
2021-11-17 18:52:53 +0900

    Add: floating animation on flicking

--- client/src/views/Main.vue
+++ client/src/views/Main.vue
@@ -6,20 +6,21 @@
             class="mt-10 mx-auto"
             color="hsl(258, 98%, 70%)"
         >
+            <DefaultBar />
             <v-carousel
                 v-model="model"
                 vertical-delimiters="right"
                 :vertical="true"
                 :show-arrows="false"
                 :hide-delimiter-background="true"
-                height="80vh"
+                height="75vh"
             >
-                <DefaultBar />
                 <v-carousel-item>
                     <v-sheet
-                        class="my-10"
+                        class="flicking mt-16"
+                        height="40vh"
+                        width="100vh"
                         color="hsl(258, 98%, 70%)"
-                        height="100vh"
                     >
                         <Flicking
                             :options="{
@@ -30,27 +31,89 @@
                             :cameraTag="'div'"
                             :plugins="plugins"
                             @move-end="onMoveEnd"
+                            class="Viewport"
                         >
-                            <div
+                            <v-card
                                 :key="i"
                                 v-for="(d, i) in surveys"
-                                class="panel mx-10"
+                                :class="[
+                                    i % 2 === 1
+                                        ? 'scrollSelect2'
+                                        : 'scrollSelect3'
+                                ]"
+                                class="panel mx-5 rounded-xl card-panel"
+                                max-width="200"
+                                max-height="300"
                             >
-                                <v-card
-                                    elevation="3"
-                                    class="mx-auto"
-                                    max-width="344"
+                                <v-img
+                                    :src="
+                                        `http://localhost:3000/download/${d.survey_image_path}`
+                                    "
+                                    height="150"
+                                ></v-img>
+
+                                <v-card-title class="text-h7">
+                                    {{ d.survey_title }}
+                                </v-card-title>
+                                <v-chip
+                                    close-icon="mdi-close-outline"
+                                    color="grey"
+                                    small
+                                    class="white--text pa-0 ml-2"
                                 >
-                                    <v-img
-                                        :src="
-                                            `http://localhost:3000/download/${d.survey_image_path}`
-                                        "
-                                        height="200px"
-                                        style="border-radius: 25% 25% 25% 25"
-                                    ></v-img>
-                                </v-card>
-                            </div>
+                                    <v-card-subtitle class="text-subtitle-2">
+                                        적립금: {{ d.survey_price }} | 쿠폰:
+                                        {{ d.survey_coupon }}
+                                    </v-card-subtitle>
+                                </v-chip>
+                                <v-card-subtitle>
+                                    <v-icon v-text="icon"></v-icon>남은 기간
+                                    <v-chip
+                                        class="ma-1 font-weight-bold text-subtitle-2"
+                                        color="deep-purple accent-3"
+                                        outlined
+                                        defalut
+                                    >
+                                        D-{{ dDays[i].dday }}
+                                    </v-chip>
+                                </v-card-subtitle>
+                            </v-card>
+                            <span
+                                slot="viewport"
+                                class="flicking-arrow-prev is-circle"
+                            ></span>
+                            <span
+                                slot="viewport"
+                                class="flicking-arrow-next is-circle"
+                            ></span>
                         </Flicking>
+                    </v-sheet>
+                    <v-sheet
+                        class="d-flex justify-sm-space-around align-center mt-5"
+                        color="hsl(258, 98%, 70%)"
+                        width="100vh"
+                    >
+                        <v-img
+                            class="custom-img"
+                            src="@/assets/img/main/mainImg5.png"
+                            aspect-ratio="1"
+                            contain
+                            height="200"
+                        ></v-img>
+                        <router-link
+                            to="/possible"
+                            style="text-decoration: none;"
+                        >
+                            <v-btn
+                                large
+                                rounded
+                                color="#Fee440"
+                                style="font-weight:bold;"
+                                class="pt-2"
+                            >
+                                시작하기
+                            </v-btn>
+                        </router-link>
                     </v-sheet>
                 </v-carousel-item>
 
@@ -114,9 +177,12 @@
 <script>
 import DefaultBar from '../layouts/default/Navbar.vue'
 import { Flicking } from '@egjs/vue-flicking'
-import { AutoPlay } from '@egjs/flicking-plugins'
+import { AutoPlay, Arrow } from '@egjs/flicking-plugins'
+import '@egjs/flicking-plugins/dist/arrow.css'
+
 const plugins = [
-    new AutoPlay({ duration: 1000, direction: 'NEXT', stopOnHover: true })
+    new AutoPlay({ duration: 4000, direction: 'NEXT', stopOnHover: true }),
+    new Arrow()
 ]
 
 export default {
@@ -128,27 +194,14 @@ export default {
             plugins,
             model: 0,
             list: [0, 1, 2, 3, 4],
-            items: [
-                {
-                    src:
-                        'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
-                },
-                {
-                    src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
-                },
-                {
-                    src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
-                },
-                {
-                    src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
-                }
-            ]
+            dDays: ''
         }
     },
     setup() {},
     mounted() {},
     created() {
         this.getSurvey()
+        this.discountDay()
     },
     unmounted() {},
     methods: {
@@ -171,6 +224,10 @@ export default {
                 window.alert('조금만 더 기다려주세요 ^^')
             }
             // this.repeat()
+        },
+        async discountDay() {
+            this.dDays = await this.$api('/date', 'get')
+            console.log(this.dDays)
         }
     }
 }
@@ -179,6 +236,22 @@ export default {
 @import '~@egjs/vue-flicking/dist/flicking.css';
 @import '~@egjs/flicking-plugins/dist/flicking-plugins.css';
 @keyframes sample-ani {
+    0% {
+        transform: translate(10, 10);
+    }
+    100% {
+        transform: translate(0, 20px);
+    }
+}
+@keyframes sample-ani2 {
+    0% {
+        transform: translate(0, 20px);
+    }
+    100% {
+        transform: translate(10, 10);
+    }
+}
+@keyframes sample-ani3 {
     0% {
         transform: translate(10, 10);
     }
@@ -203,9 +276,23 @@ export default {
     width: 120px;
     height: 150px;
     margin: auto;
-    animation: sample-ani 3s linear infinite alternate;
+    animation: sample-ani 5s linear infinite alternate;
 }
 .scrollSelect:hover {
+    animation-play-state: paused;
+}
+
+.scrollSelect2 {
+    animation: sample-ani2 2s linear infinite alternate;
+}
+.scrollSelect2:hover {
+    animation-play-state: paused;
+}
+
+.scrollSelect3 {
+    animation: sample-ani3 3s linear infinite alternate;
+}
+.scrollSelect3:hover {
     animation-play-state: paused;
 }
 .panel {
@@ -213,5 +300,29 @@ export default {
     width: 25rem;
     height: 30rem;
     border-radius: 5rem 5rem 5rem 5rem;
+}
+
+.flicking {
+    margin: 0 auto;
+}
+.Viewport {
+    height: 350px;
+}
+@keyframes sample-ani-test {
+    0% {
+        transform: translate(0, 50);
+    }
+    50% {
+    }
+    100% {
+        transform: translate(0, -50px);
+    }
+}
+
+.test {
+    align-items: center;
+    justify-content: center;
+    margin: auto;
+    animation: sample-ani-test 3s linear infinite alternate;
 }
 </style>
