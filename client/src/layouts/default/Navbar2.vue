223f150a7fb26d060af890cdefdd385ed78d368f
Heekyung Do <dhk0729@gmail.com>
2021-11-22 00:43:34 +0900

    navBar

--- client/src/layouts/default/Navbar2.vue
+++ client/src/layouts/default/Navbar2.vue
@@ -3,7 +3,7 @@
     <!-- 네비바 전체 윤곽 -->
     <div>
         <!-- drawer is true -->
-        <v-app-bar elevation="2" color="white">
+        <v-app-bar elevate-on-scroll elevation="2" color="white">
             <!-- 네비바 왼쪽 부분 -->
             <router-link class="text-decoration-none" to="/">
                 <v-toolbar-title class="font-weight-bold black--text">
